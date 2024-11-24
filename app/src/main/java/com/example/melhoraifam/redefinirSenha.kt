ece62cec66d3a7fdc91feb82196bc87a2602286d
Julia <donadellij@gmail.com>
2024-11-24 03:01:00 -0300

    Tela de cadastro finalizada e fragmento de redefinir senha criado

--- app/src/main/java/com/example/melhoraifam/redefinirSenha.kt
+++ app/src/main/java/com/example/melhoraifam/redefinirSenha.kt
@@ -1,14 +1,10 @@
 package com.example.melhoraifam
 
 import android.os.Bundle
-import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [login.newInstance] factory method to
+ * Use the [redefinirSenha.newInstance] factory method to
  * create an instance of this fragment.
  */
-class login : Fragment() {
+class redefinirSenha : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,34 +33,8 @@ class login : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
-
-        // Referenciar o EditText e o TextView para erro
-        val emailEditText = view.findViewById<EditText>(R.id.email)
-        val errorTextView = view.findViewById<TextView>(R.id.erroLogin)
-        val buttonEntrar = view.findViewById<Button>(R.id.buttonCadastrar)
-
-
-        buttonEntrar.setOnClickListener {
-
-            val email = emailEditText.text.toString()
-
-            if (email.isEmpty()) {
-
-                errorTextView.text = "O email não pode estar vazio"
-                errorTextView.visibility = View.VISIBLE // Tornar o erro visível
-            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                // Exibir erro se o email for inválido
-                errorTextView.text = "Por favor, insira um email válido"
-                errorTextView.visibility = View.VISIBLE // Tornar o erro visível
-            } else {
-                // Se o email for válido, esconder a mensagem de erro
-                errorTextView.visibility = View.GONE
-                // Proseguir com a ação (exemplo: login)
-            }
-        }
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_redefinir_senha, container, false)
     }
 
     companion object {
@@ -74,12 +44,12 @@ class login : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment login.
+         * @return A new instance of fragment redefinirSenha.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            login().apply {
+            redefinirSenha().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
